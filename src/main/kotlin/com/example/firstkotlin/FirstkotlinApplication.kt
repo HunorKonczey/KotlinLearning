8c3e3a8f0eef2d665ef1ef2c79642d2d8d6d22a6
Hunor Konczey <konczey.hunor@sonrisa.hu>
2022-03-30 09:13:44 +0300

    initial commit

---
+++ src/main/kotlin/com/example/firstkotlin/FirstkotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.firstkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FirstkotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<FirstkotlinApplication>(*args)
+}
