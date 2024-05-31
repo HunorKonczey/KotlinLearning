3d2430e06512e00d96ec1940dc27730373373af4
Hunor Konczey <konczey.hunor@sonrisa.hu>
2024-05-31 09:14:55 +0300

    Fix gson error

--- src/main/kotlin/com/example/firstkotlin/WebConfig.kt
+++ src/main/kotlin/com/example/firstkotlin/WebConfig.kt
@@ -1,26 +1,21 @@
 package com.example.firstkotlin
 
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer
-import com.google.gson.Gson
 import org.bson.types.ObjectId
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.MediaType
 import org.springframework.http.converter.HttpMessageConverter
 import org.springframework.http.converter.StringHttpMessageConverter
-import org.springframework.http.converter.json.GsonHttpMessageConverter
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
-import java.util.List
 
 
 @Configuration
 @EnableWebMvc
 class WebConfig : WebMvcConfigurer {
-
-    private val gson: Gson = Gson()
 
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
